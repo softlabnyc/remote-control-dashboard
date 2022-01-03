55af7fcfe40b487087d3e5a7d0600cfaf2c06cd7
Seth Thompson <s3th.thompson@gmail.com>
2022-01-03 17:01:03 -0500

    .

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hello, world!</title>
+        <meta name="description" content="Hello, world description." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello, world!</h1>
+      </main>
+    </div>
+  );
+}
